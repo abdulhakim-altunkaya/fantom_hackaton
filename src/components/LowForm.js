874901bd3f86998fda692ab1d0eeecf78bd83ef7
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-02-17 17:19:56 +0300

    form component is finished, more css and minor changes done

--- src/components/LowForm.js
+++ src/components/LowForm.js
@@ -10,82 +10,14 @@ function LowForm() {
     <div>
 
       <h2>SUBMIT YOUR CONTRACT</h2>
-      <p>Before submitting the contract, please make sure you contract is flattened. Flatten means if you have multiple contract files,
-        they will be packed into one file. For example, if you are using Hardhat, open CMD or Terminal
-        on your project folder, then type "npx hardhat flatten &#62; MyFlatFile.sol". This will create a flat file. Then, copy this file and paste
-        it in the area below.
+      <p>Before submitting the contract, please make sure you upload only one contract at a time. Do not upload multiple contracts.
       </p>
-      <form>
-          <textArea type="text" value={contractCode} onChange={ e => setContractCode(e.target.value) }></textArea>>
+      <form className='contractSubmitForm'>
+          <textArea type="text" value={contractCode} onChange={ e => setContractCode(e.target.value) } required></textArea>
           <input type="submit" value="Send" className='button-87'/>
       </form>
       <p>{contractCode}</p>
-      <p>Before submitting the contract, please make sure you contract is flattened. Flatten means if you have multiple contract files,
-        they will be packed into one file. For example, if you are using Hardhat, open CMD or Terminal
-        on your project folder, then type "npx hardhat flatten &#62; MyFlatFile.sol". This will create a flat file. Then, copy this file and paste
-        it in the area below.
-      </p>
-      <p>Before submitting the contract, please make sure you contract is flattened. Flatten means if you have multiple contract files,
-        they will be packed into one file. For example, if you are using Hardhat, open CMD or Terminal
-        on your project folder, then type "npx hardhat flatten &#62; MyFlatFile.sol". This will create a flat file. Then, copy this file and paste
-        it in the area below.
-      </p>
-      <p>Before submitting the contract, please make sure you contract is flattened. Flatten means if you have multiple contract files,
-        they will be packed into one file. For example, if you are using Hardhat, open CMD or Terminal
-        on your project folder, then type "npx hardhat flatten &#62; MyFlatFile.sol". This will create a flat file. Then, copy this file and paste
-        it in the area below.
-      </p>
-      <p>Before submitting the contract, please make sure you contract is flattened. Flatten means if you have multiple contract files,
-        they will be packed into one file. For example, if you are using Hardhat, open CMD or Terminal
-        on your project folder, then type "npx hardhat flatten &#62; MyFlatFile.sol". This will create a flat file. Then, copy this file and paste
-        it in the area below.
-      </p>
-      <p>Before submitting the contract, please make sure you contract is flattened. Flatten means if you have multiple contract files,
-        they will be packed into one file. For example, if you are using Hardhat, open CMD or Terminal
-        on your project folder, then type "npx hardhat flatten &#62; MyFlatFile.sol". This will create a flat file. Then, copy this file and paste
-        it in the area below.
-      </p>
-      <p>Before submitting the contract, please make sure you contract is flattened. Flatten means if you have multiple contract files,
-        they will be packed into one file. For example, if you are using Hardhat, open CMD or Terminal
-        on your project folder, then type "npx hardhat flatten &#62; MyFlatFile.sol". This will create a flat file. Then, copy this file and paste
-        it in the area below.
-      </p>
-      <p>Before submitting the contract, please make sure you contract is flattened. Flatten means if you have multiple contract files,
-        they will be packed into one file. For example, if you are using Hardhat, open CMD or Terminal
-        on your project folder, then type "npx hardhat flatten &#62; MyFlatFile.sol". This will create a flat file. Then, copy this file and paste
-        it in the area below.
-      </p>
-      <p>Before submitting the contract, please make sure you contract is flattened. Flatten means if you have multiple contract files,
-        they will be packed into one file. For example, if you are using Hardhat, open CMD or Terminal
-        on your project folder, then type "npx hardhat flatten &#62; MyFlatFile.sol". This will create a flat file. Then, copy this file and paste
-        it in the area below.
-      </p>
-      <p>Before submitting the contract, please make sure you contract is flattened. Flatten means if you have multiple contract files,
-        they will be packed into one file. For example, if you are using Hardhat, open CMD or Terminal
-        on your project folder, then type "npx hardhat flatten &#62; MyFlatFile.sol". This will create a flat file. Then, copy this file and paste
-        it in the area below.
-      </p>
-      <p>Before submitting the contract, please make sure you contract is flattened. Flatten means if you have multiple contract files,
-        they will be packed into one file. For example, if you are using Hardhat, open CMD or Terminal
-        on your project folder, then type "npx hardhat flatten &#62; MyFlatFile.sol". This will create a flat file. Then, copy this file and paste
-        it in the area below.
-      </p>
-      <p>Before submitting the contract, please make sure you contract is flattened. Flatten means if you have multiple contract files,
-        they will be packed into one file. For example, if you are using Hardhat, open CMD or Terminal
-        on your project folder, then type "npx hardhat flatten &#62; MyFlatFile.sol". This will create a flat file. Then, copy this file and paste
-        it in the area below.
-      </p>
-      <p>Before submitting the contract, please make sure you contract is flattened. Flatten means if you have multiple contract files,
-        they will be packed into one file. For example, if you are using Hardhat, open CMD or Terminal
-        on your project folder, then type "npx hardhat flatten &#62; MyFlatFile.sol". This will create a flat file. Then, copy this file and paste
-        it in the area below.
-      </p>
-      <p>Before submitting the contract, please make sure you contract is flattened. Flatten means if you have multiple contract files,
-        they will be packed into one file. For example, if you are using Hardhat, open CMD or Terminal
-        on your project folder, then type "npx hardhat flatten &#62; MyFlatFile.sol". This will create a flat file. Then, copy this file and paste
-        it in the area below.
-      </p>
-      
+
     </div>
   )
 }
--- src/components/Upperbar.js
+++ src/components/Upperbar.js
@@ -7,7 +7,10 @@ function Upperbar() {
     return (
       <div className='upperbarDiv'>
         <div>
-          <div className='upperbarLogoDiv'><img src="logo.png" alt="logo of page. You can click on it to go to main page"/></div>
+          <div className='upperbarLogoDiv'>
+            <img src="logo.png" onClick={() => navigate("/")}
+            alt="logo of page. You can click on it to go to main page"/>
+          </div>
           <div className='upperbarButtonsDiv'>
               <span className='button6' onClick={() => navigate("/membership")}>BECOME MEMBER</span>
               <span className='button6' onClick={() => navigate("/audit")}>AUDIT CONTRACT</span>
