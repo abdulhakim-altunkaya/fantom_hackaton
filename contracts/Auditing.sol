57abf18914f39b896f0758257b19f037c353aa34
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-02-19 21:54:48 +0300

    payment function and membership functions finsihed

--- contracts/Auditing.sol
+++ contracts/Auditing.sol
@@ -10,6 +10,8 @@ contract Auditing is ERC20Capped {
     event TokenMinted(address minter, uint amount); 
     event TokenBurned(address burner, uint amount); 
     event TokenTransferred(address receiver, uint amount);
+    event TokenDeposited(address sender, uint amount);
+    event NewMember(address caller);
 
     //1. OWNERSHIP OPERATIONS
     address public owner;
@@ -51,6 +53,8 @@ contract Auditing is ERC20Capped {
         _burn(msg.sender, _amount*(10**18));
         emit TokenBurned(msg.sender, _amount);
     }
+
+    //owner can withdraw excess tokens ("profit") to his metamask account.
     function transferToken(uint _amount) external onlyOwner {
         require(balanceOf(address(this)) > 0, "contract doesn't have CONTOR");
         require(msg.sender == tx.origin, "contracts cannot withdraw");
@@ -58,35 +62,85 @@ contract Auditing is ERC20Capped {
         _transfer(address(this), msg.sender, _amount*(10**18));
         emit TokenTransferred(msg.sender, _amount);
     }
+
+    //Contract must have enough tokens. So that people can come and buy tokens for auditing service. 
+    uint lastReplenishment = block.timestamp;
+    function replenishTreasury(uint _amount) external onlyOwner {
+        require( block.timestamp > lastReplenishment + 15 seconds, "Replenishment cannot be often");//in production: 5 days
+        require(_amount < 2000, "Replenishment must be limited amount");
+        lastReplenishment = block.timestamp;
+        _mint(address(this), _amount*(10**18));
+    }
+
+    //view functions
     function getTotalSupply() external view returns(uint) {
         return totalSupply() / (10**18);
     }
-    function getContractAddress() external view returns(address) {
+    function getCAddress() external view returns(address) {
         return address(this);
     }
     function getYourBalance() external view returns(uint) {
         return balanceOf(msg.sender) / (10**18);
     }
-    function getContractBalance() external view returns(uint) {
+    function getCBalToken() external view returns(uint) {
         return balanceOf(address(this)) / (10**18);
+    }
+    function getCBalEther() external view returns(uint) {
+        return address(this).balance;
     }
 
     //3.PAYMENT OPERATIONS
-    uint fee = 5;
-    function makePayment() external {
-        require(balanceOf(msg.sender) > 0, "you don't have CONTOR");
+    uint fee = 1;
+    function changeFee(uint _fee) external onlyOwner {
+        fee = _fee;
+    }
+    function makePayment() external returns(bool) {
+        require(balanceOf(msg.sender) >= fee, "you don't have CONTOR");
         require(msg.sender == tx.origin, "contracts cannot withdraw");
         require(msg.sender != address(0), "real addresses can withdraw");
-        _transfer(address(this), msg.sender, _amount*(10**18));
-        emit TokenTransferred(msg.sender, _amount);
+        _transfer(msg.sender, address(this), fee*(10**18));
+        emit TokenDeposited(msg.sender, fee);
+        return true;
+    }
+    function buyToken() external payable {
+        require(balanceOf(address(this)) >= 1000, "contract does not have enough CONTOR");
+        require(msg.sender == tx.origin, "only accounts can buy tokens, not contracts");
+        require(msg.sender != address(0), "real addresses can buy");
+        //here the buyer send amount(with decimals) to the contract by using msg.value
+        require(msg.value == 10000000000000000, "deposit is 10 finney (0,01 ether)" ); 
+        //For 0,01 FTM, we are transferring 10 CONTOR.
+        _transfer(address(this), msg.sender, 10*(10**18));
+        emit TokenTransferred(msg.sender, 10);
     }
 
-    address[] internal membersArray;
+
+
+    //4.MEMBERSHIP OPERATIONS
+    modifier isMember() {
+        if(membersMapping[msg.sender] == true) {
+            revert("already a member");
+        }
+        _;
+    }
+
     mapping(address => bool) internal membersMapping;
-    function becomeMember(uint amount) external {
-
+    function becomeMember(uint amount) external isMember {
+        require(balanceOf(msg.sender) >= 10, "you must have at least 10 CONTOR");
+        require(msg.sender == tx.origin, "only accounts can become member, not contracts");
+        require(msg.sender != address(0), "address 0 cant become member");
+        _transfer(msg.sender, address(this), 10*(10**18));
+        membersMapping[msg.sender] = true;
+        emit NewMember(msg.sender);
     }
 
+    function checkMembership() external view returns(bool) {
+        return membersMapping[msg.sender];
+    }
+    function leaveMembership() external {
+        require(msg.sender == tx.origin, "only accounts remove membership");
+        require(msg.sender != address(0), "address 0 is already not a member");
+        membersMapping[msg.sender] = false;
+    }
     /*
     -withdraw/transfer tokens
     -withdraw fantom */
